eb1a0e54e5d106e09f988013646c21f5e191f88b
Robin Boers <robindev2019@outlook.com>
2023-03-01 21:32:17 +0100

    Clean up mix.exs

--- mix.exs
+++ mix.exs
@@ -4,12 +4,13 @@ defmodule UnLibD.MixProject do
   def project do
     [
       name: "Unlibrary daemon",
-      source_url: "https://github.com/unlibrary/readerd",
+      source_url: "https://github.com/libre0b11/unlibd",
       app: :unlibd,
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps() ++ check_deps(),
+      package: package()
     ]
   end
 
@@ -23,12 +24,23 @@ defmodule UnLibD.MixProject do
 
   defp deps do
     [
-      {:unlib, path: "../reader"},
+      {:unlib, path: "../reader"}
+    ]
+  end
 
-      # CI/CD
+  defp check_deps do
+    [
       {:ex_check, "~> 0.14.0", only: [:dev], runtime: false},
       {:credo, ">= 0.0.0", only: [:dev], runtime: false},
       {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Robin Boers"],
+      organization: "0b11",
+      links: %{github: "https://github.com/libre0b11/unlib"}
     ]
   end
 end
